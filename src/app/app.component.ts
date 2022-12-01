bb285eb214b9b40a63f87431739b403fa3dfece5
Ali Chavoshi <alichavoshii1372@gmail.com>
2022-12-01 12:09:38 +0330

    4- Handle User (Logout)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { AccountService } from './account/account.service';
+import { IUser } from './shared/models/user';
 import { environment } from 'src/environments/environment';
 import { BasketService } from './basket/basket.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'EduStoreClient';
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.getBasket();
+    this.getCurrentUser();
+  }
+
+  private getCurrentUser() {
+    const user = <IUser>JSON.parse(localStorage.getItem(environment.keySaveUser));
+    if (user) {
+      this.accountService.setCurrentUser(user);
+    }
   }
 
   private getBasket() {
--- src/app/core/layers/navbar/navbar.component.ts
+++ src/app/core/layers/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
+import { AccountService } from './../../../account/account.service';
 import { BasketService } from './../../../basket/basket.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IBasket } from 'src/app/shared/models/basket';
+import { IUser } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +12,15 @@ import { IBasket } from 'src/app/shared/models/basket';
 })
 export class NavbarComponent implements OnInit {
   basket$: Observable<IBasket>;
+  currentUser$: Observable<IUser>;
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basketItems$;
+    this.currentUser$ = this.accountService.currentUser$;
+  }
+  logout() {
+    this.accountService.logout();
   }
 }
