e4f393b4274881c920c0b120c393030784dc079c
Ali Chavoshi <alichavoshii1372@gmail.com>
2022-12-16 17:21:21 +0330

    8- Register Component

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,5 +1,8 @@
+import { Register } from './../../shared/models/user';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private bcService: BreadcrumbService) {}
-
+  constructor(private bcService: BreadcrumbService, private accountService: AccountService) {}
+  form = new FormGroup({
+    phoneNumber: new FormControl('', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]),
+    password: new FormControl(''),
+    displayName: new FormControl('')
+  });
   ngOnInit(): void {
     this.bcService.set('@register', 'ثبت نام در سایت');
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.accountService.register(<Register>this.form.value).subscribe();
   }
 }
