260d71ecfaec15b4b8a44ff5aedf9e2fe4e3bf5d
Ali Chavoshi <alichavoshii1372@gmail.com>
2023-08-02 13:50:26 +0330

    6-ReviewBasket

--- src/app/checkout/checkout-review/checkout-review.component.ts
+++ src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasket } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-checkout-review',
@@ -6,10 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-review.component.scss']
 })
 export class CheckoutReviewComponent implements OnInit {
-
-  constructor() { }
+  basket$: Observable<IBasket>;
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basketItems$;
   }
-
 }
